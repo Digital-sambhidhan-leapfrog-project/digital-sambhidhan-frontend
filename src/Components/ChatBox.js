b8f4eb5a0049ccd2bbf02cd09524e531fd0e2e4b
sunayanapandey44 <077sunayana044.sunayana@sagarmatha.edu.np>
2023-11-04 20:40:25 +0545

    Fixed empty blob,implemented chat thread

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -1,103 +1,94 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ChatBox.css";
 import Blob from "./Blob";
-import Logo from "./myLogo.png";
-class ChatBox extends Component {
-  // const [components, setComponents] = useState([])
-  newComponent = <Blob text="Hello Human" />;
-  constructor(props) {
-    super(props);
-    this.state = {
-      components: [this.newComponent],
-      input: "",
-      inputarr: [],
-      questions: [
-        "What are my rights?",
-        "What freedom do we get as citizens?",
-        "How much taxes does the law say we need to pay?",
-      ],
-      answer: "",
-    };
+import userLogo from "./userLogo.png"; 
+import answerLogo from "./myLogo.png"; 
+
+function ChatBox(props) {
+  const [components, setComponents] = useState([]);
+  const [input, setInput] = useState("");
+  const [questions] = useState([
+    "What are my rights?",
+    "What freedom do we get as citizens?",
+    "How much taxes does the law say we need to pay?",
+  ]);
+  const [answer, setAnswer] = useState("");
+  const [chatItems, setChatItems] = useState([
+    { text: "Hello Human", isQuestion: false }, 
+  ]);
+
+  const addChatItem = (item, isQuestion) => {
+    setChatItems((prevItems) => [...prevItems, { text: item, isQuestion }]);
   };
 
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { input } = this.state;
-    if (input.trim() !== "") {
-      this.setState({
-        answer: `Look over section I for "${input}"`,
-      });
-      this.props.onNewMessage(input);
+  useEffect(() => {
+    if (answer !== "") {
+      addChatItem(answer, false); 
     }
-    const newComponent = <Blob text={this.state.answer} />;
-    this.setState((prevState) => ({
-      components: [...prevState.components, newComponent],
-    }));
-  };
+  }, [answer]);
 
-  handleQuestionClick = (question) => {
-    this.setState({
-      input: question,
-      answer: "",
-      questions: [],
-    });
-  };
-
-  render() {
-    return (
-      <div className="chat-box" id="chat-box">
-        {/* <div className="image-container">
-          <img
-            src={Image4} // Use the imported image
-            alt="Image"
-          />
-        </div> */}
-        {/* <div className="answer-placeholder" id="answer-placeholder">
-          <div className="answer-text">
-            {this.state.answer || "Answer will appear here"}
-          </div>
-        </div> */}
-        {this.state.components.map((component, index) => (
-        <div className="chatBlob">
-          <div className="chatImage">
-            <img src={Logo} alt="Image"/> 
-          </div>
-        {/* <Blob text = {this.state.answer || "Hello Human!!! How can I help you?"}/> */}
-        
-          <div key={index}>{component}</div>
-       
+  useEffect(() => {
+    const chatComponents = chatItems.map((item, index) => (
+      <div
+        className={`chatBlob ${item.isQuestion ? "question" : "answer"}`}
+        key={index}
+      >
+        <div className="chatImage">
+          <img src={item.isQuestion ? userLogo : answerLogo} alt="Image" />
         </div>
-         ))}
-        <div className="Chat">
-          <div className="suggestions">
-            {this.state.questions.map((question, index) => (
-              <button
-                key={index}
-                onClick={() => this.handleQuestionClick(question)}
-                className="question-button"
-              >
-                {question}
-              </button>
-            ))}
-            </div>
-            <div className="query-box">
-            <form onSubmit={this.handleSubmit} className="input-form">
-              <input
-                id="Queries"
-                type="text"
-                value={this.state.input}
-                // onChange={(e) => this.setState({ input: e.target.value })}
-                placeholder="Enter your queries here"
-                autoComplete="off"
-              />
-              <button type="submit">Send</button>
-            </form>
-            </div>
-          </div>
+        <div>{<Blob text={item.text} />}</div>
       </div>
-    );
-  }
+    ));
+    setComponents(chatComponents);
+  }, [chatItems]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim() !== "") {
+      addChatItem(input, true); 
+      setTimeout(() => {
+        addChatItem(`Look at section I for "${input}"`, false); 
+      }, 1000);
+      setInput(""); 
+    }
+  };
+
+  const handleQuestionClick = (question) => {
+    setInput(question);
+    setAnswer("");
+  };
+
+  return (
+    <div className="chat-box" id="chat-box">
+      {components}
+      <div className="Chat">
+        <div className="suggestions">
+          {questions.map((question, index) => (
+            <button
+              key={index}
+              onClick={() => handleQuestionClick(question)}
+              className="question-button"
+            >
+              {question}
+            </button>
+          ))}
+        </div>
+        <div className="query-box">
+          <form onSubmit={handleSubmit} className="input-form">
+            <input
+              id="Queries"
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Enter your queries here"
+              autoComplete="off"
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatBox;
